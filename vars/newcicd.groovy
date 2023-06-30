aa8e7e2749812eed67eae94f81a031a1cd5a23bd
sriman114 <136916349+sriman114@users.noreply.github.com>
2023-06-30 10:25:59 +0530

    Update newcicd.groovy

--- vars/newcicd.groovy
+++ vars/newcicd.groovy
@@ -12,3 +12,10 @@ def dply(jobname, ip, contextpath)
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }
+
+def tst(jobname)
+{
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+        
+
